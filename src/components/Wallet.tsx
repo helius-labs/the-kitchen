8f4cb592f9579fc0d3e3f33d1ba8413a90e8e6f9
Evan <0xIchigo@protonmail.com>
2023-10-10 14:56:56 -0400

    Fix Styling For Wallet Connect Modal

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,16 +1,18 @@
 import { FC } from "react";
 import {
   WalletModalProvider,
-  WalletMultiButton,
+  WalletMultiButton
 } from "@solana/wallet-adapter-react-ui";
 import "../index.css";
 
-require("@solana/wallet-adapter-react-ui/styles.css");
+//require("@solana/wallet-adapter-react-ui/styles.css");
+
+//const WalletMultiButtonDynamic = lazy(() => import('@solana/wallet-adapter-react-ui').then(module => ({ default: module.WalletMultiButton })));
 
 export const Wallet: FC = () => {
   return (
     <WalletModalProvider>
-      <WalletMultiButton/>
+        <WalletMultiButton />
     </WalletModalProvider>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{js,jsx,ts,tsx}", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"];
 
-export const content = [
-  "./src/**/*.{js,jsx,ts,tsx}",
-  'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      orange: "#E84125",
+      "orange-hover": "rgb(234, 88, 12, 0.8)",
+      "off-black": "rgb(7, 7, 7, 0.5)",
+    },
+  },
 };
-export const plugins = [require('flowbite/plugin')
-];
 
+export const plugins = [require("flowbite/plugin")];
